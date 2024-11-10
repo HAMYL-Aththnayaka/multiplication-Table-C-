21d1c82e735c3454af6208a15fd20bfff4d77014
Yasas Aththanayaka <Aththanayakayasas@gmail.com>
2024-11-10 20:03:50 +0530

    Add files via upload

---
+++ Multiplication Table/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A Multiplication Table ");
+            Console.WriteLine();
+
+            Console.Write("Enter a number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= 10; i++) {
+                
+                Console.WriteLine("{0} x {1} = {2} ",i,number,i*number);
+            
+            
+            }
+
+
+
+
+             Console.ReadKey();
+        }
+    }
+}
